0e1c97c404ca702ff1701f4e39b9db83c1d32235
dhrumilpathak <52490348+dhrumilpathak@users.noreply.github.com>
2021-12-10 15:02:17 +0530

    Add files via upload

---
+++ PhoneNumberValidation/PhoneNumberVaidation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PhoneNumberValidation
+{
+    class PhoneNumberVaidation
+    {
+
+        public string FormatePhoneNumber(long number)
+         {
+           
+            string formatedPhoneNumber = number.ToString();
+            int a = formatedPhoneNumber.Length;
+            if (a == 10)
+            {
+                formatedPhoneNumber = Regex.Replace(formatedPhoneNumber, @"(\d{3})(\d{3})(\d{4})", "$1-$2-$3");
+            
+            }
+            else 
+            {
+                throw new Exception("The Phone number entered should be of 10 Digit only");
+            }
+
+            return formatedPhoneNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Phone Number");
+            long phonenumber = Convert.ToInt64(Console.ReadLine());
+
+            PhoneNumberVaidation p1 = new PhoneNumberVaidation();
+
+            Console.WriteLine(p1.FormatePhoneNumber(phonenumber));
+            Console.ReadLine();
+
+        }
+    }
+}
+
